267251ba91719636b658b47aa7a55e9483d140e7
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-09-14 08:53:21 -0300

    Creating symbol table struct and function headers

---
+++ header/symbol_table.h
@@ -0,0 +1,16 @@
+typedef struct Node node;
+
+struct Node {
+
+	char *symbol;
+	char *scope;
+	node *next;
+}
+/* Creating list */
+node *create_list(node *list);
+/* Verify if list is empty, return 0 to false, and 1 to true */
+int is_empty(node *list);
+/* Insert a variable name in list */
+void insert_symbol(node *list, char *symbol, node *new_node);
+/* Search the variable name in the same scope */
+int find_symbol(node *list, char *symbol, char *scope);
